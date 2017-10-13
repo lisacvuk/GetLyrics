c88d8991f7b8b0310622a9f7b97df80a0c23868f
Stefan Vukanović <lisacvukhome@gmail.com>
2017-10-13 20:28:10 +0200

    Make it store raw lyrics in lyrics_raw, and processed ones in lyrics. Also check if directories exist first, before making them.

--- getsong.py
+++ getsong.py
@@ -2,14 +2,14 @@ import sys
 import html
 
 f = open(sys.argv[1], 'r') # Open the file which is the first cmd arg
-
+f_out = open(sys.argv[2], 'w') # Open the output file
 searchStart = "<div class='lyricbox'>" # String at the start of the lyrics
 searchEnd = "<div class='lyricsbreak'>" # String at the end of the lyrics
 
 endline = "" # The result
 
-for line in f: 
-	if line.find(searchStart) != -1: 
+for line in f:
+	if line.find(searchStart) != -1:
 		end = line.find(searchEnd) # Get the end location
 		start = line.find(searchStart) # Get the start location
 		endline = line[len(searchStart):end] # Cut the text from in between
@@ -17,3 +17,4 @@ for line in f:
 endline = "\n".join(endline.split("<br />")) # Replace HTML codes with newlines
 
 print(html.unescape(endline)) # Replace HTML codes with ASCII letters
+f_out.write(html.unescape(endline))
--- getsong.sh
+++ getsong.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
-mkdir lyrics
-cd lyrics
+if [ ! -d "lyrics_raw" ]; then
+	mkdir lyrics_raw
+fi
+if [ ! -d "lyrics" ]; then
+	mkdir lyrics
+fi
+cd lyrics_raw
 name=$1
 name="${name// /_}"
 song=$2
 song="${song// /_}"
 wget -q http://lyrics.wikia.com/wiki/$name:$song
 cd ..
-python3 getsong.py lyrics/$name:$song
+python3 getsong.py lyrics_raw/$name:$song lyrics/$name:$song
